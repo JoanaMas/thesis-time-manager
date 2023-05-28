5c3390d3c7ace723e72c105de312f495d4fa752e
JoanaMas <joana.mastianica@gmail.com>
2023-05-28 09:20:49 +0300

    1: Project initiated, work environment set up completed

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// Pages
+import HomePage from './pages/HomePage';
+// Style
+import './main.scss';
+
+const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <HomePage />
+    }
+]);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />  
+  </React.StrictMode>,
+);
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+import { FC, ReactElement } from 'react';
+
+const HomePage: FC = (): ReactElement => {
+  return (
+    <div>HomePage</div>
+  );
+};
+
+export default HomePage;
