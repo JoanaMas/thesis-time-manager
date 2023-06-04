0a0916e3249a28fc39ed2093c4c307bfd11baa91
JoanaMas <joana.mastianica@gmail.com>
2023-06-04 11:12:33 +0300

    23: Aside component UI fixed

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 // Slices
-import errorReducer from './features/errorSlice';
 import userDataReducer from './features/userDataSlice';
 import pagesReducer from './features/pagesSlice';
 import productivityTimeReducer from './features/productivityTimeSlice';
@@ -8,7 +7,6 @@ import productivityTimeReducer from './features/productivityTimeSlice';
 // STATE MANAGEMENT
 export const store = configureStore({
     reducer: {
-        error: errorReducer,
         userData: userDataReducer,
         pages: pagesReducer,
         productivityTime: productivityTimeReducer,
